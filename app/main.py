1a1627032afbc0c317809177e7d6bd7e73135ff3
Stephany Valderrama Carmona <svalderrama4@gmail.com>
2023-02-15 15:02:40 +0100

    API rest para universidad virtual con Docker

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
-# app/main.py
-
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="Api rest en contenedor")
+from app.api.api_v1.api import api_router
+from app.core.config import settings
 
+app = FastAPI(
+    title=settings.PROJECT_NAME, description=settings.DESCRIPTION,
+    version=settings.VERSION,openapi_url=f"{settings.API_V1_STR}/openapi.json"
+)
 
-@app.get("/")
-def read_root():
-    return {"hello": "world"}
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-
+app.include_router(api_router, prefix=settings.API_V1_STR)
