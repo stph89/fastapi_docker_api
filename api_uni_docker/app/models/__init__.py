e8d3084c5cfe97dd0746a2397031c4c07acb226c
Stephany Valderrama Carmona <svalderrama4@gmail.com>
2023-02-16 10:41:45 +0100

    APi con docker y postgres funciona

--- api_uni_docker/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .item import Item
-from .user import User
-from .asignatura import Asignatura
-from app.models.alumno import Alumno
-from .matricula import matricula
-from .profesor import Profesor
-
-
