f196f2c533852cdd2ac93d4cca51e902f77db331
Stephany Valderrama Carmona <svalderrama4@gmail.com>
2023-02-16 10:11:20 +0100

    Rama para arreglar la conexión con FastApi en el docker.

--- alembic/env.py
+++ alembic/env.py
@@ -9,55 +9,25 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
-"""config ---> Es el objeto para la configuración del Alembic, 
-el cual extraerá los valores de las variables de entorno y la 
-información del archivo .ini del archivo el uso"""
 
 config = context.config
-
-"""Esta linea es para configurar el loggers"""
 fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-# target_metadata = None
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app.db.base import Base  # noqa
+from app.db.base import Base
 
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 dotenv.load_dotenv(verbose=True)
-
 
 def get_url():
     user = os.getenv("POSTGRES_USER", "admin_f5")
     password = os.getenv("POSTGRES_PASSWORD", "")
     server = os.getenv("POSTGRES_SERVER", "13.38.159.23")
-    db = os.getenv("POSTGRES_DB", "demo_official")
+    db = os.getenv("POSTGRES_DB", "docker_university")
     return f"postgresql://{user}:{password}@{server}/{db}"
 
 
 def run_migrations_offline():
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
     url = get_url()
     context.configure(
         url=url, target_metadata=target_metadata, literal_binds=True, compare_type=True
@@ -66,14 +36,7 @@ def run_migrations_offline():
     with context.begin_transaction():
         context.run_migrations()
 
-
 def run_migrations_online():
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
     configuration = config.get_section(config.config_ini_section)
     configuration["sqlalchemy.url"] = get_url()
     connectable = engine_from_config(
